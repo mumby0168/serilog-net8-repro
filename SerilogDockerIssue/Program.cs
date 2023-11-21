ec425ef4d4fce0d0ebbf54b0d7032eb82ac163c3
Billy Mumby <billy.mumby@outlook.com>
2023-11-21 21:19:43 +0000

    feat: repro bug

---
+++ SerilogDockerIssue/Program.cs
@@ -0,0 +1,30 @@
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+try
+{
+    Log.Information("Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+            .ReadFrom
+            .Configuration(context.Configuration));
+
+    var app = builder.Build();
+
+    app.MapGet("/", () => "Hello World!");
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
